4636e55791e417b8ee4eb33dbfbcb890afc00a60
kulandairaj <dk.kulandai@gmail.com>
2018-10-12 11:29:21 -0400

    updated the latest changes

--- lib/mymaplib.component.d.ts
+++ lib/mymaplib.component.d.ts
@@ -1,5 +1,7 @@
-import { ElementRef, OnInit } from '@angular/core';
+import { ElementRef, OnInit, EventEmitter } from '@angular/core';
 export declare class MymaplibComponent implements OnInit {
+    ticketList: any;
+    ticketClick: EventEmitter<any>;
     title: string;
     base64Image: string;
     someInput: ElementRef;
